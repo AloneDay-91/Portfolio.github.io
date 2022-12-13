acfa6cb477dbb350615e8cc560412dbb5fe37187
bruz0003 <elouan.bruzek@etudiant.univ-reims.fr>
2022-12-13 23:19:38 +0100

    Commit initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bgcolor': '1b1620'
+      }
+    },
+    fontFamily: {
+      fontpers: ['Albert Sans', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
